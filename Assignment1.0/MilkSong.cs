64dc67bc27259c581bbc93e92e214ce0bdc54037
Yamkela24 <151846985+Yamkela24@users.noreply.github.com>
2023-12-08 12:11:08 +0200

    first commit

---
+++ Assignment1.0/MilkSong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._0
+{
+    internal class MilkSong
+    {
+        public MilkSong() 
+        {
+            for (int i = 99; i > 1; i--)
+            {
+                Console.WriteLine($"{i} bottles of milk on the wall, {i} bottles of milk");
+
+
+                if (i == 2)
+                {
+                    Console.WriteLine($"Take one down and pass it around, {i - 1} bottle on the wall");
+                }
+                else
+                {
+                    Console.WriteLine($"Take one down and pass it around, {i - 1} bottles on the wall");
+                }
+
+                // Console.WriteLine($"{i} bottles of milk on the wall");
+            }
+            Console.WriteLine($"1 bottle of milk on the wall, 1 bottle of milk");
+        }
+    }
+}
---
+++ Assignment1.0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MilkSong milkSong = new MilkSong();
+
+            Console.WriteLine(milkSong);
+           
+
+        }
+
+
+    }
+}
